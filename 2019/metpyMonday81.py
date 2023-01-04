5b93f065d3f664cb03d8027a72df04f6977ba4b2
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2023-01-04 13:46:27 -0300

    Add files via upload

---
+++ 2019/metpyMonday81.py
@@ -0,0 +1,48 @@
+
+# - Metpy Monday
+# - 81 - Managing Environments
+# - https://www.youtube.com/watch?v=nzkJs-niH6Q
+
+# - Commands to create, clone, activate, deactivate, use and clean environments
+
+# conda env list
+
+# conda activate unidata
+
+# conda deactivate
+
+# conda activate python-testing
+
+# conda activate unidata
+
+# conda deactivate
+
+# conda create --name my_new_paper python=3
+
+# conda activate my_new_paper
+
+# conda list
+
+# conda install numpy
+
+# conda list
+
+# conda uninstall numpy
+
+# conda deactivate
+
+# conda env remove --name my_new_paper
+
+# conda env list
+
+# conda create --name env1
+
+# conda create --name env2 --clone env1
+
+# conda env remove --name env1
+
+# conda env remove --name env2
+
+# conda cleam -tp
+
+
